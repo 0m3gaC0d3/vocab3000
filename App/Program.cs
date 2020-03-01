7681d040e600118cb04e845ee5363c674dec9092
Wolf Utz <wpu@hotmail.de>
2020-03-01 20:30:28 +0100

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int SomeMethod()
+        {
+            return 123;
+        }
+    }
+}
